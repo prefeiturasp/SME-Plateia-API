d38fa1cd7e4bf48af9a4adaa8f0931f663126768
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2022-04-20 11:26:19 -0300

    Delete SME-API-Plateia directory

--- SME-API-Plateia/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace SME_API_Plateia.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
