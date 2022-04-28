ef18c4ad1164287f5aee9c8ea7d76b232b1c4f1e
Erik Silva <erik.silva@amcom.com.br>
2022-04-28 13:25:09 -0300

    Primeiro commit!

---
+++ src/DataDB/CargoUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SME_API_Plateia.DataDB
+{
+    public partial class CargoUser
+    {
+        public Guid Id { get; set; }
+        public long CodigoCargo { get; set; }
+        public string? NomeCargo { get; set; }
+        public Guid IdUser { get; set; }
+        public byte State { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
